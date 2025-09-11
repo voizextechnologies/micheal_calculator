bde307ad8395ac70cce33a802cc74fd90e56ccaa
voizextechnologies <voizextechnologies@gmail.com>
2025-09-11 16:03:30 +0530

    Updated tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,3 @@
-@tailwind base;
-@tailwind components;
-@tailwind utilities;
-
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
